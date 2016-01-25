8f0ac87c987d025fbb2448d09bc4d055de1486d4
JasonSaari <Jsaari@outlook.com>
2016-01-25 14:25:11 -0500

    Inital Commit

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee
+    {
+        //Backing fields
+        private string firstName;
+        private string lastName;
+        private decimal weeklySalary;
+
+        //Properties for the backing fields
+        public string FristName
+        {
+            get { return firstName; }
+            set { firstName = value;  }
+            
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return weeklySalary; }
+            set { weeklySalary = value; }
+
+        }
+
+        //One Method that is public
+        public string GetFullName()
+        {            
+            return this.firstName + " " + this.lastName;
+        }
+
+        //Override method
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName + " " + this.weeklySalary.ToString("C");
+        }
+
+
+
+        //One Method that is private.  Can only be called inside of this class
+        private string foo()
+        {
+            return "FOO";
+        }
+
+        //3 parameter constructor
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            //Assign the passed in values to the fields
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+            //assign the passed in value by the property
+            this.WeeklySalary = weeklySalary;
+        }
+
+        //Default constructor 
+        public Employee()
+        { 
+            //Left Blank on purpose
+        }
+
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,6 +10,78 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
+
+            Employee employee1 = new Employee("Dave", "Barnes", 537.00m);
+            Employee employee2 = new Employee("Joe", "Somebody", 125.50m);
+
+            //Create simple int that will be used for value vs reference
+            int myNumber = 5;
+
+
+            //Write the value of the int before the method, call the method, print after call
+            Console.WriteLine(myNumber);
+            ChangeAnInt(myNumber);
+            Console.WriteLine(myNumber);
+
+            //Write the value of the int before the method, call the method, print after call
+            Console.WriteLine(employee1.ToString());
+            ChangeAnObject(employee1);
+            Console.WriteLine(employee1.ToString());
+           
+            //To illistrate ToString Override.
+            //Console.WriteLine(employee.GetFullName());
+            //Console.WriteLine(employee.ToString());
+
+            //Showing how to use an array with objects
+            Employee[] employees = new Employee[10];
+
+            //Instanciate some employees into the array
+            employees[0] = new Employee("James", "Kirk", 453.00m);
+            employees[1] = new Employee("Jean-Luc", "Picard", 290.00m);
+            employees[2] = new Employee("Benjamin", "Sisko", 578.00m);
+            employees[3] = new Employee("Kathryn", "Janeway", 194.00m);
+            employees[4] = new Employee("Sterlin", "Archer", 394.00m);
+
+
+            //A for each loop that will loop through each element fo the Employees array            
+            foreach (Employee employee in employees)
+            {
+                //Check to make sure that the current object is not null.
+                //We know that the first 5 have values becasuse we assigned them right above 
+                //but the last 5 are null, so we better put in a check.
+                if (employee != null)
+                {
+                    //output the information of the employee
+                    Console.WriteLine(employee.ToString());
+                }
+            }
+
+
+
+
+
+
         }
+
+        //Method take in an interger, which is passed by value 
+        //and the changes the value of it.
+        static void ChangeAnInt(int myNumber)
+        {
+            myNumber = 456;
+        }    
+
+        //Method thake in an Employee class, which is passed by reference 
+        //and the changes a property of it. 
+        static void ChangeAnObject(Employee employee)
+        {
+            employee.FristName = "Jason";
+
+        }
+    
+    
+    
+    
     }
+
+
 }
